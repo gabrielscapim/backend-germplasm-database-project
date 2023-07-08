d0a9572d25f44c4df4526d074582381ae9e8893a
Gabriel <scapimgabriel@gmail.com>
2023-07-08 16:42:19 -0300

    feat: entidade germoplasma

---
+++ api/src/main/java/germplasm/database/api/model/Germplasm.java
@@ -0,0 +1,254 @@
+package germplasm.database.api.model;
+
+import jakarta.persistence.*;
+
+@Table(name = "germplasms")
+@Entity(name = "Germplasm")
+public class Germplasm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="deletado")
+    private Boolean deletado;
+
+    @Column(name="nome")
+    private String nome;
+
+    @Column(name="tipo_de_material_genetico")
+    private String tipoDeMaterialGenetico;
+
+    @Column(name = "textura_do_grao")
+    private String texturaDoGrao;
+
+    @Column(name = "origem")
+    private String origem;
+
+    @Column(name = "transgenico")
+    private Boolean transgenico;
+
+    @Column(name = "eventos_transgenicos")
+    private String eventosTransgenicos;
+
+    @Column(name = "local_na_camara_fria")
+    private String localNaCamaraFria;
+
+    @Column(name = "data_de_entrada")
+    private String dataDeEntrada;
+
+    @Column(name = "data_da_ultima_colheita")
+    private String dataDaUltimaColheita;
+
+    @Column(name = "colletotrichum_graminicola")
+    private Float colletotrichumGraminicola;
+
+    @Column(name = "puccinia_sorghi")
+    private Float pucciniaSorghi;
+
+    @Column(name = "exserohilum_turcicum")
+    private Float exserohilumTurcicum;
+
+    @Column(name = "phaeosphaeria_maydis")
+    private Float phaeosphaeriaMaydis;
+
+    @Column(name = "puccinia_polysora")
+    private Float pucciniaPolysora;
+
+    @Column(name = "physopella_zeae")
+    private Float physopellaZeae;
+
+    @Column(name = "corn_stunt")
+    private Float cornStunt;
+
+    @Column(name = "diplodia_maydis")
+    private Float diplodiaMaydis;
+
+    @Column(name = "fusarium_moniliforme")
+    private Float fusariumMoniliforme;
+
+    @Column(name = "gibberella_zeae")
+    private Float gibberellaZeae;
+
+    @Column(name = "altura_de_planta")
+    private Float alturaDePlanta;
+
+    @Column(name = "altura_de_espiga")
+    private Float alturaDeEspiga;
+
+    @Column(name = "diametro_da_espiga")
+    private Float diametroDaEspiga;
+
+    @Column(name = "numero_de_graos_por_fileira")
+    private Float numeroDeGraosPorFileira;
+
+    @Column(name = "numero_de_fileiras_de_graos_por_espiga")
+    private Float numeroDeFileirasDeGraosPorEspiga;
+
+    @Column(name = "comprimento_de_espigas")
+    private Float comprimentoDeEspigas;
+
+    @Column(name = "rendimento_de_graos")
+    private Float rendimentoDeGraos;
+
+    @Column(name = "capacidade_de_expansao")
+    private Float capacidadeDeExpansao;
+
+    @Column(name = "massa_de_cem_graos")
+    private Float massaDeCemGraos;
+
+    @Column(name = "numero_de_espigas")
+    private Float numeroDeEspigas;
+
+    @Column(name = "diametro_do_colmo")
+    private Float diametroDoColmo;
+
+    @Column(name = "comprimento_de_grao")
+    private Float comprimentoDeGrao;
+
+    @Column(name = "largura_de_grao")
+    private Float larguraDeGrao;
+
+    @Column(name = "espessura_de_grao")
+    private Float espessuraDeGrao;
+
+    @Column(name = "espessura_do_pericarpo")
+    private Float espessuraDoPericarpo;
+
+    @Column(name = "difusividade_termica_do_pericarpo")
+    private Float difusividadeTermicaDoPericarpo;
+
+    @Column(name = "teor_de_proteina")
+    private Float teorDeProteina;
+
+    @Column(name = "teor_de_oleo")
+    private Float teorDeOleo;
+
+    @Column(name = "amido_total")
+    private Float amidoTotal;
+
+    @Column(name="primeira_folha_pigmentacao_antocianinica_da_bainha")
+    private Integer primeiraFolhaPigmentacaoAntocianinicaDaBainha;
+
+    @Column(name="primeira_folha_forma_do_apice")
+    private Integer primeiraFolhaFormaDoApice;
+
+    @Column(name="folhagem_intensidade_da_cor_verde")
+    private Integer folhagemIntensidadeDaCorVerde;
+
+    @Column(name="folha_ondulacao_da_margem_da_lamina")
+    private Integer folhaOndulacaoDaMargemDaLamina;
+
+    @Column(name="folha_angulo_entre_a_lamina_e_o_colmo")
+    private Integer folhaAnguloEntreALaminaEOColmo;
+
+    @Column(name="folha_curvatura_da_lamina")
+    private Integer folhaCurvaturaDaLamina;
+
+    @Column(name="colmo_grau_do_zig_zag")
+    private Integer colmoGrauDoZigZag;
+
+    @Column(name="pendao_ciclo_ate_a_antese")
+    private Integer pendaoCicloAteAAntese;
+
+    @Column(name="pendao_pigmentacao_antocianinica_na_base_da_gluma")
+    private Integer pendaoPigmentacaoAntocianinicaNaBaseDaGluma;
+
+    @Column(name="pendao_pigmentacao_antocianinica_da_gluma_excluida_a_base")
+    private Integer pendaoPigmentacaoAntocianinicaDaGlumaExcluidaABase;
+
+    @Column(name="pendao_pigmentacao_antocianinica_das_anteras")
+    private Integer pendaoPigmentacaoAntocianinicaDasAnteras;
+
+    @Column(name="pendao_angulo_entre_o_eixo_principal_e_os_ramos_laterais")
+    private Integer pendaoAnguloEntreOEixoPrincipalEOsRamosLaterais;
+
+    @Column(name="pendao_curvatura_dos_ramos_laterais")
+    private Integer pendaoCurvaturaDosRamosLaterais;
+
+    @Column(name="pendao_numero_de_ramificacoes_primarias")
+    private Integer pendaoNumeroDeRamificacoesPrimarias;
+
+    @Column(name="espiga_ciclo_ate_o_aparecimento_dos_estigmas")
+    private Integer espigaCicloAteOAparecimentoDosEstigmas;
+
+    @Column(name="espiga_pigmentacao_antocianinica_dos_estigmas")
+    private Integer espigaPigmentacaoAntocianinicaDosEstigmas;
+
+    @Column(name="colmo_pigmentacao_antocianinica_das_raizes_aereas")
+    private Integer colmoPigmentacaoAntocianinicaDasRaizesAereas;
+
+    @Column(name="pendao_densidade_das_espiguetas")
+    private Integer pendaoDensidadeDasEspiguetas;
+
+    @Column(name="folha_pigmentacao_antocianinica_da_bainha")
+    private Integer folhaPigmentacaoAntocianinicaDaBainha;
+
+    @Column(name="colmo_pigmentacao_antocianinica_dos_entrenos")
+    private Integer colmoPigmentacaoAntocianinicaDosEntrenos;
+
+    @Column(name="pendao_comp_do_eixo_principal_acima_do_ramo_lateral_mais_baixo")
+    private Integer pendaoCompDoEixoPrincipalAcimaDoRamoLateralMaisBaixo;
+
+    @Column(name="pendao_comp_do_eixo_principal_acima_do_ramo_lateral_mais_alto")
+    private Integer pendaoCompDoEixoPrincipalAcimaDoRamoLateralMaisAlto;
+
+    @Column(name="pendao_comp_do_ramo_lateral")
+    private Integer pendaoCompDoRamoLateral;
+
+    @Column(name="planta_comp")
+    private Integer plantaComp;
+
+    @Column(name="planta_relacao_alt_ins_pedunculo_espiga_mais_alta_e_comp_planta")
+    private Integer plantaRelacaoAltInsPedunculoEspigaMaisAltaECompPlanta;
+
+    @Column(name="folha_largura_da_lamina")
+    private Integer folhaLarguraDaLamina;
+
+    @Column(name="pedunculo_comp")
+    private Integer pedunculoComp;
+
+    @Column(name="espiga_comp")
+    private Integer espigaComp;
+
+    @Column(name="espiga_diametro_no_meio_da_espiga")
+    private Integer espigaDiametroNoMeioDaEspiga;
+
+    @Column(name="espiga_forma")
+    private Integer espigaForma;
+
+    @Column(name="espiga_numero_de_fileiras_de_graos")
+    private Integer espigaNumeroDeFileirasDeGraos;
+
+    @Column(name="espiga_numero_de_cores_dos_graos")
+    private Integer espigaNumeroDeCoresDosGraos;
+
+    @Column(name="grao_intensidade_da_cor_amarela")
+    private Integer graoIntensidadeDaCorAmarela;
+
+    @Column(name="grao_comp")
+    private Integer graoComp;
+
+    @Column(name="grao_largura")
+    private Integer graoLargura;
+
+    @Column(name="espiga_tipo_de_grao")
+    private Integer espigaTipoDeGrao;
+
+    @Column(name="espiga_enrugamento_do_extremo_superior_do_grao")
+    private Integer espigaEnrugamentoDoExtremoSuperiorDoGrao;
+
+    @Column(name="espiga_cor_do_extremo_superior_do_grao")
+    private Integer espigaCorDoExtremoSuperiorDoGrao;
+
+    @Column(name="espiga_cor_do_lado_dorsal_do_grao")
+    private Integer espigaCorDoLadoDorsalDoGrao;
+
+    @Column(name="tipo_de_grao_estourado")
+    private Integer tipoDeGraoEstourado;
+
+    @Column(name="espiga_pigmentacao_antocianinica_das_glumas_do_sabugo")
+    private Integer espigaPigmentacaoAntocianinicaDasGlumasDoSabugo;
+
+    @Column(name="informacoes_adicionais")
+    private String informacoesAdicionais;
+}
