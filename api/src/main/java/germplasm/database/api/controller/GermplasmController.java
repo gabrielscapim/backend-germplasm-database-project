262c5cc1458bf83064d9b6789c4fc0ef74aa48b6
Gabriel <scapimgabriel@gmail.com>
2023-07-10 14:04:19 -0300

    feat: page no método GET

--- api/src/main/java/germplasm/database/api/controller/GermplasmController.java
+++ api/src/main/java/germplasm/database/api/controller/GermplasmController.java
@@ -6,22 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/germplasm")
 public class GermplasmController {
 
     @Autowired
-    private GermplasmRepository germplasmRepository;
-
-    @Autowired
     private GermplasmService germplasmService;
 
     @GetMapping
-    public ResponseEntity getAllGermplasms() {
-        return ResponseEntity.ok(germplasmService.getAllGermplasms());
+    public ResponseEntity getAllGermplasms(@PageableDefault(size = 20, sort={ "id" }) Pageable pageable) {
+        return ResponseEntity.ok(germplasmService.getAllGermplasms(pageable));
     }
+
 }
--- api/src/main/java/germplasm/database/api/service/GermplasmService.java
+++ api/src/main/java/germplasm/database/api/service/GermplasmService.java
@@ -1,14 +1,11 @@
 package germplasm.database.api.service;
 
 import germplasm.database.api.dto.DataDetailingGermplasmDTO;
-import germplasm.database.api.model.Germplasm;
 import germplasm.database.api.repository.GermplasmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class GermplasmService {
@@ -16,12 +13,9 @@ public class GermplasmService {
     @Autowired
     GermplasmRepository germplasmRepository;
 
-    public Iterable<DataDetailingGermplasmDTO> getAllGermplasms() {
-        Iterable<Germplasm> germplasms = germplasmRepository.findAll();
-        List<DataDetailingGermplasmDTO> dtos = new ArrayList<>();
+    public Page<DataDetailingGermplasmDTO> getAllGermplasms(Pageable pageable) {
+        var germplasmPage = germplasmRepository.findAll(pageable).map(DataDetailingGermplasmDTO::new);
 
-        germplasms.forEach(germplasm -> dtos.add(new DataDetailingGermplasmDTO(germplasm)));
-
-        return dtos;
+        return germplasmPage;
     }
 }
