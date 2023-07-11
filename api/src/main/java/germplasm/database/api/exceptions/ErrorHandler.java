6022625c736669ee97c28d1db20bccf9540aaa2e
Gabriel <scapimgabriel@gmail.com>
2023-07-11 16:21:02 -0300

    feat: erro para germoplasma não encontrado

--- api/src/main/java/germplasm/database/api/exceptions/ErrorHandler.java
+++ api/src/main/java/germplasm/database/api/exceptions/ErrorHandler.java
@@ -11,6 +11,9 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class ErrorHandler {
 
@@ -20,6 +23,14 @@ public class ErrorHandler {
         var errorsMessages = fieldErrorList.stream().map(ErrorMessage::new).toList();
 
         return ResponseEntity.badRequest().body(errorsMessages);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity entityNotFoundError() {
+        Map<String, String> message = new HashMap<String, String>();
+        message.put("message", "Germoplasma não encontrado");
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
     }
 
     private record ErrorMessage(String message) {
