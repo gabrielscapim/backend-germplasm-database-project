26413328bef71ab8bccae2e49a8977933ab63b3f
Gabriel <scapimgabriel@gmail.com>
2023-07-12 08:53:15 -0300

    feat: criação da classe autentication service

--- api/src/main/java/germplasm/database/api/repository/UserRepository.java
+++ api/src/main/java/germplasm/database/api/repository/UserRepository.java
@@ -2,6 +2,8 @@ package germplasm.database.api.repository;
 
 import germplasm.database.api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    UserDetails findByLogin(String username);
 }
