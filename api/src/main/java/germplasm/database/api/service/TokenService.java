0ae561ed751e859b5b53938dd06c3e165ebfe5dd
Gabriel <scapimgabriel@gmail.com>
2023-07-12 09:54:16 -0300

    feat: security filter e security configurations para habilitar qualquer requisição no login

--- api/src/main/java/germplasm/database/api/service/TokenService.java
+++ api/src/main/java/germplasm/database/api/service/TokenService.java
@@ -3,6 +3,7 @@ package germplasm.database.api.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import germplasm.database.api.model.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -35,4 +36,16 @@ public class TokenService {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 
+    public String getSubject(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("Germplasm API")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token inválido ou expirado", exception);
+        }
+    }
 }
